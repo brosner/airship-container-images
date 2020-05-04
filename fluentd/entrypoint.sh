4cc5f305b046f9048b564a35b9e9cd57bcc70196
Brian Rosner <brian@brosner.com>
2020-05-04 14:07:51 -0600

    clean entrypoint and create log dir

--- fluentd/entrypoint.sh
+++ fluentd/entrypoint.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
-#source vars if file exists
+if [[ ! -d /var/log/fluentd ]]; then
+  mkdir /var/log/fluentd
+fi
+
 DEFAULT=/etc/default/fluentd
 
 if [ -r $DEFAULT ]; then
-    set -o allexport
-    . $DEFAULT
-    set +o allexport
+  set -o allexport
+  . $DEFAULT
+  set +o allexport
 fi
 
-# If the user has supplied only arguments append them to `fluentd` command
+# if the user has supplied only arguments append them to `fluentd` command
 if [ "${1#-}" != "$1" ]; then
-    set -- fluentd "$@"
+  set -- fluentd "$@"
 fi
 
-# If user does not supply config file or plugins, use the default
+# if user does not supply config file or plugins, use the default
 if [ "$1" = "fluentd" ]; then
-    if ! echo $@ | grep ' \-c' ; then
-       set -- "$@" -c /fluentd/etc/${FLUENTD_CONF}
-    fi
-
-    if ! echo $@ | grep ' \-p' ; then
-       set -- "$@" -p /fluentd/plugins
-    fi
+  if ! echo $@ | grep ' \-c' ; then
+    set -- "$@" -c /fluentd/etc/${FLUENTD_CONF}
+  fi
+  if ! echo $@ | grep ' \-p' ; then
+    set -- "$@" -p /fluentd/plugins
+  fi
 fi
 
 exec "$@"
